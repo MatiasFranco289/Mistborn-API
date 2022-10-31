d4d19da60ec4ee95d97c0744b719f5be5902e343
Matias Franco <matias.franco289@gmail.com>
2022-10-31 00:26:13 -0300

    fix for deploy

--- src/functions/connect_db.js
+++ src/functions/connect_db.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql');
 
 const connection = mysql.createConnection({
-    host: process.env.DB_HOST || 'localhost',
+    host: process.env.DB_HOST,
     database:process.env.DB_NAME,
     user:process.env.DB_USER,
     password: process.env.DB_PASSWORD
