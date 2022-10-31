c718cfbcb7cd8abe650e038f5fbdb50960c702be
Matias Franco <matias.franco289@gmail.com>
2022-10-31 00:31:03 -0300

    more changes for prod

--- src/functions/connect_db.js
+++ src/functions/connect_db.js
@@ -4,13 +4,15 @@ const connection = mysql.createConnection({
     host: process.env.DB_HOST,
     database:process.env.DB_NAME,
     user:process.env.DB_USER,
-    password: process.env.DB_PASSWORD
+    password: process.env.DB_PASSWORD,
+    port: process.env.PORT
 });
 
 console.log(`db host ${process.env.DB_HOST}`);
 console.log(`db name ${process.env.DB_NAME}`);
 console.log(`db user ${process.env.DB_USER}`);
 console.log(`db pas ${process.env.DB_PASSWORD}`);
+console.log(`db PORT ${process.env.PORT}`);
 
 connection.connect((error) => {
     if(error){
